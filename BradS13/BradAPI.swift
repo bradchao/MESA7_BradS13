4d946a45c37b2c5cd5498ebf53a4f465a7f0ea1b
iii <iii@userteki-MacBook.local>
2017-06-19 12:02:24 +0800

    v2

---
+++ BradS13/BradAPI.swift
@@ -0,0 +1,38 @@
+//
+//  BradAPI.swift
+//  BradS13
+//
+//  Created by iii on 2017/6/19.
+//  Copyright © 2017年 iii. All rights reserved.
+//
+
+import Foundation
+
+class Brad01 {
+    static var counter = 0;
+    init(){
+        print("Brad01:init()")
+        Brad01.counter += 1
+    }
+    deinit {
+        print("Brad01:deinit()")
+        Brad01.counter -= 1
+    }
+}
+
+
+class Brad02 {
+    var obj3:Brad03?
+    func m1(){print("Brad02:m1()")}
+    deinit {
+        print("Brad02:die")
+    }
+}
+
+class Brad03 {
+    weak var obj2:Brad02?
+    func m1(){print("Brad03:m1()")}
+    deinit {
+        print("Brad03:die")
+    }
+}
